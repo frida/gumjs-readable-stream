3ce6895dc4f9b38a9cd843421bc3dc36b92150de
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-10-09 01:11:21 +0200

    Move to ESM and assume we're in a Frida runtime

--- errors.js
+++ errors.js
@@ -1,116 +1,105 @@
-'use strict';
+import { format } from 'util';
 
-const codes = {};
+const messages = new Map();
+export const codes = {};
 
-function createErrorType(code, message, Base) {
-  if (!Base) {
-    Base = Error
-  }
-
-  function getMessage (arg1, arg2, arg3) {
-    if (typeof message === 'string') {
-      return message
-    } else {
-      return message(arg1, arg2, arg3)
+export function aggregateTwoErrors(innerError, outerError) {
+  if (innerError && outerError && innerError !== outerError) {
+    if (Array.isArray(outerError.errors)) {
+      // If `outerError` is already an `AggregateError`.
+      outerError.errors.push(innerError);
+      return outerError;
     }
+    // eslint-disable-next-line no-restricted-syntax
+    const err = new AggregateError([
+      outerError,
+      innerError,
+    ], outerError.message);
+    err.code = outerError.code;
+    return err;
   }
-
-  class NodeError extends Base {
-    constructor (arg1, arg2, arg3) {
-      super(getMessage(arg1, arg2, arg3));
-    }
-  }
-
-  NodeError.prototype.name = Base.name;
-  NodeError.prototype.code = code;
-
-  codes[code] = NodeError;
+  return innerError || outerError;
 }
 
-// https://github.com/nodejs/node/blob/v10.8.0/lib/internal/errors.js
-function oneOf(expected, thing) {
-  if (Array.isArray(expected)) {
-    const len = expected.length;
-    expected = expected.map((i) => String(i));
-    if (len > 2) {
-      return `one of ${thing} ${expected.slice(0, len - 1).join(', ')}, or ` +
-             expected[len - 1];
-    } else if (len === 2) {
-      return `one of ${thing} ${expected[0]} or ${expected[1]}`;
-    } else {
-      return `of ${thing} ${expected[0]}`;
-    }
-  } else {
-    return `of ${thing} ${String(expected)}`;
+function makeNodeErrorWithCode(Base, key) {
+  return function NodeError(...args) {
+    const error = new Base();
+    const message = getMessage(key, args, error);
+    Object.defineProperties(error, {
+      message: {
+        value: message,
+        enumerable: false,
+        writable: true,
+        configurable: true,
+      },
+      toString: {
+        value() {
+          return `${this.name} [${key}]: ${this.message}`;
+        },
+        enumerable: false,
+        writable: true,
+        configurable: true,
+      },
+    });
+    error.code = key;
+    return error;
+  };
+}
+
+function E(sym, val, def, ...otherClasses) {
+  messages.set(sym, val);
+  def = makeNodeErrorWithCode(def, sym);
+
+  if (otherClasses.length !== 0) {
+    otherClasses.forEach((clazz) => {
+      def[clazz.name] = makeNodeErrorWithCode(clazz, sym);
+    });
+  }
+  codes[sym] = def;
+}
+
+function getMessage(key, args, self) {
+  const msg = messages.get(key);
+
+  if (typeof msg === 'function') {
+    return Reflect.apply(msg, self, args);
+  }
+
+  const expectedLength = (msg.match(/%[dfijoOs]/g) || []).length;
+  if (args.length === 0)
+    return msg;
+
+  args.unshift(msg);
+  return Reflect.apply(format, null, args);
+}
+
+export class AbortError extends Error {
+  constructor() {
+    super('The operation was aborted');
+    this.code = 'ABORT_ERR';
+    this.name = 'AbortError';
   }
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
-function startsWith(str, search, pos) {
-	return str.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
-function endsWith(str, search, this_len) {
-	if (this_len === undefined || this_len > str.length) {
-		this_len = str.length;
-	}
-	return str.substring(this_len - search.length, this_len) === search;
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
-function includes(str, search, start) {
-  if (typeof start !== 'number') {
-    start = 0;
-  }
-
-  if (start + search.length > str.length) {
-    return false;
-  } else {
-    return str.indexOf(search, start) !== -1;
-  }
-}
-
-createErrorType('ERR_INVALID_OPT_VALUE', function (name, value) {
-  return 'The value "' + value + '" is invalid for option "' + name + '"'
-}, TypeError);
-createErrorType('ERR_INVALID_ARG_TYPE', function (name, expected, actual) {
-  // determiner: 'must be' or 'must not be'
-  let determiner;
-  if (typeof expected === 'string' && startsWith(expected, 'not ')) {
-    determiner = 'must not be';
-    expected = expected.replace(/^not /, '');
-  } else {
-    determiner = 'must be';
-  }
-
-  let msg;
-  if (endsWith(name, ' argument')) {
-    // For cases like 'first argument'
-    msg = `The ${name} ${determiner} ${oneOf(expected, 'type')}`;
-  } else {
-    const type = includes(name, '.') ? 'property' : 'argument';
-    msg = `The "${name}" ${type} ${determiner} ${oneOf(expected, 'type')}`;
-  }
-
-  msg += `. Received type ${typeof actual}`;
-  return msg;
-}, TypeError);
-createErrorType('ERR_STREAM_PUSH_AFTER_EOF', 'stream.push() after EOF');
-createErrorType('ERR_METHOD_NOT_IMPLEMENTED', function (name) {
-  return 'The ' + name + ' method is not implemented'
-});
-createErrorType('ERR_STREAM_PREMATURE_CLOSE', 'Premature close');
-createErrorType('ERR_STREAM_DESTROYED', function (name) {
-  return 'Cannot call ' + name + ' after a stream was destroyed';
-});
-createErrorType('ERR_MULTIPLE_CALLBACK', 'Callback called multiple times');
-createErrorType('ERR_STREAM_CANNOT_PIPE', 'Cannot pipe, not readable');
-createErrorType('ERR_STREAM_WRITE_AFTER_END', 'write after end');
-createErrorType('ERR_STREAM_NULL_VALUES', 'May not write null values to stream', TypeError);
-createErrorType('ERR_UNKNOWN_ENCODING', function (arg) {
-  return 'Unknown encoding: ' + arg
-}, TypeError);
-createErrorType('ERR_STREAM_UNSHIFT_AFTER_END_EVENT', 'stream.unshift() after end event');
-
-module.exports.codes = codes;
+E('ERR_EVENT_RECURSION', 'The event "%s" is already being dispatched', Error);
+E('ERR_ILLEGAL_CONSTRUCTOR', 'Illegal constructor', TypeError);
+E('ERR_INVALID_ARG_TYPE', 'Invalid argument type', TypeError);
+E('ERR_INVALID_ARG_VALUE', 'Invalid argument value', TypeError, RangeError);
+E('ERR_INVALID_RETURN_VALUE', 'Invalid return value', TypeError, RangeError);
+E('ERR_INVALID_THIS', 'Value of "this" must be of type %s', TypeError);
+E('ERR_METHOD_NOT_IMPLEMENTED', 'The %s method is not implemented', Error);
+E('ERR_MISSING_ARGS', 'Missing argument', TypeError);
+E('ERR_MULTIPLE_CALLBACK', 'Callback called multiple times', Error);
+E('ERR_OUT_OF_RANGE', 'Out of range', RangeError);
+E('ERR_STREAM_ALREADY_FINISHED',
+  'Cannot call %s after a stream was finished',
+  Error);
+E('ERR_STREAM_CANNOT_PIPE', 'Cannot pipe, not readable', Error);
+E('ERR_STREAM_DESTROYED', 'Cannot call %s after a stream was destroyed', Error);
+E('ERR_STREAM_NULL_VALUES', 'May not write null values to stream', TypeError);
+E('ERR_STREAM_PREMATURE_CLOSE', 'Premature close', Error);
+E('ERR_STREAM_PUSH_AFTER_EOF', 'stream.push() after EOF', Error);
+E('ERR_STREAM_UNSHIFT_AFTER_END_EVENT',
+  'stream.unshift() after end event', Error);
+E('ERR_STREAM_WRITE_AFTER_END', 'write after end', Error);
+E('ERR_UNKNOWN_ENCODING', 'Unknown encoding: %s', TypeError);
--- readable.js
+++ readable.js
@@ -1,9 +1,45 @@
-exports = module.exports = require('./lib/_stream_readable.js');
-exports.Stream = exports;
-exports.Readable = exports;
-exports.Writable = require('./lib/_stream_writable.js');
-exports.Duplex = require('./lib/_stream_duplex.js');
-exports.Transform = require('./lib/_stream_transform.js');
-exports.PassThrough = require('./lib/_stream_passthrough.js');
-exports.finished = require('./lib/internal/streams/end-of-stream.js');
-exports.pipeline = require('./lib/internal/streams/pipeline.js');
+import { addAbortSignal } from './lib/add-abort-signal.js';
+import compose from './lib/compose.js';
+import { destroyer } from './lib/destroy.js';
+import Duplex from './lib/duplex.js';
+import eos from './lib/end-of-stream.js';
+import PassThrough from './lib/passthrough.js';
+import pipeline from './lib/pipeline.js';
+import * as promises from './lib/promises.js';
+import Readable from './lib/readable.js';
+import Transform from './lib/transform.js';
+import { isDisturbed } from './lib/utils.js';
+import Writable from './lib/writable.js';
+
+import { promisify } from 'util';
+
+export default Readable;
+export {
+  isDisturbed,
+  Readable as Stream,
+  Readable,
+  Writable,
+  Duplex,
+  Transform,
+  PassThrough,
+  pipeline,
+  addAbortSignal,
+  eos as finished,
+  destroyer as destroy,
+  compose,
+  promises,
+};
+
+Object.defineProperty(pipeline, promisify.custom, {
+  enumerable: true,
+  get() {
+    return promises.pipeline;
+  }
+});
+
+Object.defineProperty(eos, promisify.custom, {
+  enumerable: true,
+  get() {
+    return promises.finished;
+  }
+});
